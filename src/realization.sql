62222ed2eb1aebdf662ed15a64f7fc113927ccb3
Evgenia Sadykova <79652953633@yandex.ru>
2022-05-02 21:48:08 +0200

    Update realization.sql

--- src/realization.sql
+++ src/realization.sql
@@ -34,42 +34,61 @@ CREATE TABLE analysis.dm_rfm_segments (
         monetary_value INT CHECK (monetary_value BETWEEN 1 AND 5 ) );
 
 -- SQL-запрос для заполнения витрины
-INSERT INTO analysis.dm_rfm_segments
-(user_id, recency, frequency, monetary_value)
+-- INSERT INTO analysis.dm_rfm_segments
+-- (user_id, recency, frequency, monetary_value)
+-- WITH t AS (
+-- SELECT DISTINCT u.id AS user_id,
+-- CAST((MAX(order_ts) OVER ()) AS DATE) - CAST((MAX(CASE WHEN os.key = 'Closed' THEN order_ts ELSE NULL END) OVER(PARTITION BY u.id)) AS DATE) days_FROM_last_order,
+-- COUNT(CASE WHEN os.key = 'Closed' THEN order_ts ELSE NULL END) OVER (PARTITION BY u.id) orders_cnt,
+-- COALESCE(SUM(CASE WHEN os.key = 'Closed' THEN payment ELSE NULL END) OVER (PARTITION BY u.id),0) AS payment_BY_user
+-- FROM analysis.Users u
+-- LEFT JOIN analysis.Orders o
+-- ON u.id = o.user_id
+-- LEFT JOIN analysis.OrderStatuses os
+-- ON o.status=os.id
+-- WHERE EXTRACT(YEAR FROM order_ts) >= 2021 
+-- ) 
+-- SELECT 
+--  DISTINCT user_id,
+--  CASE 
+--  WHEN row_number() OVER (order BY days_FROM_last_order desc) between 1 and 200 THEN 1 
+--  WHEN row_number() OVER (order BY days_FROM_last_order desc) between 201 and 400 THEN 2
+--  WHEN row_number() OVER (order BY days_FROM_last_order desc) between 401 and 600 THEN 3
+--  WHEN row_number() OVER (order BY days_FROM_last_order desc) between 601 and 800 THEN 4
+--  ELSE 5
+--  END AS recency,
+--  CASE 
+--  WHEN row_number() OVER (order BY orders_cnt) between 1 and 200 THEN 1 
+--  WHEN row_number() OVER (order BY orders_cnt) between 201 and 400 THEN 2
+--  WHEN row_number() OVER (order BY orders_cnt) between 401 and 600 THEN 3
+--  WHEN row_number() OVER (order BY orders_cnt) between 601 and 800 THEN 4
+--  ELSE 5 END AS frequency
+-- , CASE 
+--  WHEN row_number() OVER (order BY payment_BY_user) between 1 and 200 THEN 1 
+--  WHEN row_number() OVER (order BY payment_BY_user) between 201 and 400 THEN 2
+--  WHEN row_number() OVER (order BY payment_BY_user) between 401 and 600 THEN 3
+--  WHEN row_number() OVER (order BY payment_BY_user) between 601 and 800 THEN 4
+--  ELSE 5 END AS monetary_value
+-- FROM t;
+
+-- SQL-запрос для заполнения витрины
+
 WITH t AS (
-SELECT DISTINCT u.id AS user_id,
-CAST((MAX(order_ts) OVER ()) AS DATE) - CAST((MAX(CASE WHEN os.key = 'Closed' THEN order_ts ELSE NULL END) OVER(PARTITION BY u.id)) AS DATE) days_FROM_last_order,
-COUNT(CASE WHEN os.key = 'Closed' THEN order_ts ELSE NULL END) OVER (PARTITION BY u.id) orders_cnt,
-COALESCE(SUM(CASE WHEN os.key = 'Closed' THEN payment ELSE NULL END) OVER (PARTITION BY u.id),0) AS payment_BY_user
-FROM analysis.Users u
-LEFT JOIN analysis.Orders o
-ON u.id = o.user_id
-LEFT JOIN analysis.OrderStatuses os
-ON o.status=os.id
-WHERE EXTRACT(YEAR FROM order_ts) >= 2021 
-) 
 SELECT 
- DISTINCT user_id,
- CASE 
- WHEN row_number() OVER (order BY days_FROM_last_order desc) between 1 and 200 THEN 1 
- WHEN row_number() OVER (order BY days_FROM_last_order desc) between 201 and 400 THEN 2
- WHEN row_number() OVER (order BY days_FROM_last_order desc) between 401 and 600 THEN 3
- WHEN row_number() OVER (order BY days_FROM_last_order desc) between 601 and 800 THEN 4
- ELSE 5
- END AS recency,
- CASE 
- WHEN row_number() OVER (order BY orders_cnt) between 1 and 200 THEN 1 
- WHEN row_number() OVER (order BY orders_cnt) between 201 and 400 THEN 2
- WHEN row_number() OVER (order BY orders_cnt) between 401 and 600 THEN 3
- WHEN row_number() OVER (order BY orders_cnt) between 601 and 800 THEN 4
- ELSE 5 END AS frequency
-, CASE 
- WHEN row_number() OVER (order BY payment_BY_user) between 1 and 200 THEN 1 
- WHEN row_number() OVER (order BY payment_BY_user) between 201 and 400 THEN 2
- WHEN row_number() OVER (order BY payment_BY_user) between 401 and 600 THEN 3
- WHEN row_number() OVER (order BY payment_BY_user) between 601 and 800 THEN 4
- ELSE 5 END AS monetary_value
-FROM t;
+distinct u.id as user_id, 
+CAST((MAX(order_ts) OVER ()) AS DATE) - CAST((MAX(order_ts) OVER(PARTITION BY u.id)) AS DATE) days_from_last_order,
+COUNT(order_ts) OVER (PARTITION BY u.id) orders_cnt,
+SUM(payment) OVER (PARTITION BY u.id) AS payment_by_user
+FROM analysis.Users u
+LEFT JOIN (SELECT * FROM analysis.orders 
+WHERE EXTRACT(YEAR FROM order_ts) >= 2021 AND status = 4) o
+ON u.id = o.user_id)
+
+SELECT DISTINCT user_id, 
+NTILE(5) OVER (ORDER BY days_from_last_order DESC NULLS FIRST) AS recency,
+NTILE(5) OVER (ORDER BY orders_cnt NULLS FIRST) AS frequency,
+NTILE(5) OVER (ORDER BY payment_by_user NULLS FIRST) AS monetary_value
+FROM t
 
 -- Доработка представления
 CREATE OR REPLACE VIEW analysis.Orders AS 
